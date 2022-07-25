09208a2347f00b00b16b6dbd229439558f262314
Mustafa Eren Hızarcı <mrgenerator3@gmail.com>
2022-07-25 17:14:18 +0300

    animation changed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -13,12 +13,19 @@ const Todo = ({
   return (
     <motion.li
       id={id}
-      exit={{margin:0,padding:0,height:0,opacity:0,color:"transparent"}}
-      animate={{ opacity: [0, 1]}}
-      transition={{duration:0.5}}
+      exit={{
+        margin: 0,
+        padding: 0,
+        height: 0,
+        opacity: 0,
+        color: "transparent",scale:[1,0]
+      }}
+      animate={{ opacity: [0, 1] }}
+      transition={{ duration: 0.5 }}
       className={`flex justify-center max-h-max items-center w-full rounded-md my-1 drop-shadow-md ${bgColor}`}
     >
-      <input
+      <motion.input
+        exit={{ height: 0 }}
         onChange={onCheckboxChange}
         className="scale-150 px-3 my-2 mr-3 w-1/6 dark:bg-gray-300"
         type="checkbox"
@@ -33,18 +40,20 @@ const Todo = ({
       >
         {content}
       </p>
-      <button
+      <motion.button
+        exit={{ height: 0,color:"transparent" }}
         onClick={enterEditMode}
         className="text-blue-700 w-1/6 text-lg rounded-full bg-white dark:bg-gray-300 mx-2 px-1 "
       >
         <i className="fa-solid fa-pen-to-square"></i>
-      </button>
-      <button
+      </motion.button>
+      <motion.button
+        exit={{ height: 0,color:"transparent" }}
         onClick={onClickDelete}
         className=" text-red-600 text-lg w-1/6 rounded-full bg-white dark:bg-gray-300 mx-2 px-1"
       >
         <i className="fa-solid fa-trash w-full"></i>
-      </button>
+      </motion.button>
     </motion.li>
   );
 };
